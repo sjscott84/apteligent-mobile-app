00f992db0d0f69052bd144ce327a523b3dd59145
Sarah Scott <s.j.scott84@gmail.com>
2016-10-07 15:54:35 -0700

    Add crash filter types (currently non-functional

--- src/components/crashInfo.js
+++ src/components/crashInfo.js
@@ -27,6 +27,26 @@ class CrashInfo extends Component {
           <Summary what={'Crash Rate'} timeFrame={'Last 24h'} figure={this.props.crashPercent} change={0.5} />
           <Summary what={'Crash Count'} timeFrame={'Last 24h'} figure={numeral(this.props.crashCount).format('0.0a')} change={-0.34} />
         </View>
+        <View style={styles.app}>
+          <Text style={styles.dark18Text}>NEW CRASH GROUPS IN ALL VERSIONS</Text>
+          <Text style={styles.light14Text}>Last 24 Hours</Text>
+          <View style={[{flexDirection: 'row'}, {justifyContent:'flex-start'}]}>
+            <Icon.Button name="user" size={20} color='rgb(122,143,147)' backgroundColor='white'>
+              <Text style={styles.light14Text}>Users Affected</Text>
+            </Icon.Button>
+            <Icon.Button name="bar-chart" size={20} color='rgb(122,143,147)' backgroundColor='white'>
+              <Text style={styles.light14Text}>Total Occurances</Text>
+            </Icon.Button>
+          </View>
+          <View style={[{flexDirection: 'row'}, {justifyContent:'flex-start'}]}>
+            <Icon.Button name="calendar-o" size={20} color='rgb(122,143,147)' backgroundColor='white'>
+              <Text style={styles.light14Text}>First Occured</Text>
+            </Icon.Button>
+            <Icon.Button name="clock-o" size={20} color='rgb(122,143,147)' backgroundColor='white'>
+              <Text style={styles.light14Text}>Last Seen</Text>
+            </Icon.Button>
+          </View>
+        </View>
       </View>
     )
   };
@@ -43,7 +63,7 @@ class Summary extends Component {
         <Text style={styles.dark15Text}>{this.props.what}</Text>
         <Text style={styles.light14Text}>{this.props.timeFrame}</Text>
         <View style={{flexDirection: 'row'}}>
-          <Text style={styles.boldText}>{this.props.figure}</Text>
+          <Text style={styles.boldText}>{this.props.figure+'%'}</Text>
           <Triangle change={this.props.change}/>
           <Text style={[styles.light11Text, {marginTop: 4.5}]}>{this.props.change}%</Text>
         </View>
