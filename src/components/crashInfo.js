e98f3aa044eb53e100aee241187b6d98373d1d95
Sarah Scott <s.j.scott84@gmail.com>
2016-10-07 15:08:29 -0700

    Add summary numbers to crash info

--- src/components/crashInfo.js
+++ src/components/crashInfo.js
@@ -10,6 +10,8 @@ import {
 
 import styles from './styleSheet';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Triangle from './triangle';
+import numeral from 'numeral'
 
 
 class CrashInfo extends Component {
@@ -21,6 +23,10 @@ class CrashInfo extends Component {
           <Text style={styles.dark18Text}>{this.props.name}</Text>
           <Icon.Button name="cog" size={20} color='rgb(98,129,133)' backgroundColor='white' onPress={this._onPressBack.bind(this)} />
         </View>
+        <View style={[styles.app, styles.crashInfo]}>
+          <Summary what={'Crash Rate'} timeFrame={'Last 24h'} figure={this.props.crashPercent} change={0.5} />
+          <Summary what={'Crash Count'} timeFrame={'Last 24h'} figure={numeral(this.props.crashCount).format('0.0a')} change={-0.34} />
+        </View>
       </View>
     )
   };
@@ -29,5 +35,21 @@ class CrashInfo extends Component {
     this.props.navigator.pop();
   }
 }
+
+class Summary extends Component {
+  render(){
+    return(
+      <View style={[styles.appDetailSummaryItem, styles.border]}>
+        <Text style={styles.dark15Text}>{this.props.what}</Text>
+        <Text style={styles.light14Text}>{this.props.timeFrame}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.boldText}>{this.props.figure}</Text>
+          <Triangle change={this.props.change}/>
+          <Text style={[styles.light11Text, {marginTop: 4.5}]}>{this.props.change}%</Text>
+        </View>
+      </View>
+    )
+  }
+};
 
 module.exports = CrashInfo;
