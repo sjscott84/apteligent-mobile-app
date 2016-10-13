9551437583c84eb52f2c8e42ae40c3029d8666ff
Sarah Scott <s.j.scott84@gmail.com>
2016-10-13 14:24:54 -0700

    Add a list of crashes by version. TODO: Style this list...

--- src/components/crashDetail.js
+++ src/components/crashDetail.js
@@ -38,21 +38,31 @@ class CrashDetail extends Component {
           <Text style={styles.dark18Text}>{this.props.name}</Text>
           <Icon.Button name="cog" size={20} color='rgb(98,129,133)' backgroundColor='white' onPress={this._onPressBack.bind(this)} />
         </View>
-        <View style={styles.app}>
-          <Text style={styles.dark15Text}>CRASH DETAILS</Text>
-          <Text style={styles.bold15Text}>{this.props.crashName}</Text>
-          <Text style={styles.dark15Text}>{this.props.reason}</Text>
-          <Text style={styles.dark15Text}>Status: {this.props.status}</Text>
-          <Text style={styles.dark15Text}>Occurred: {this.props.occurances} times</Text>
-          <Text style={styles.dark15Text}>Affected: {this.props.users} users</Text>
-          <Text style={styles.dark15Text}>Last Occured: {moment(this.props.lastOccured).format('DD MMM YYYY h:mm:ss a')}</Text>
-          <Text style={styles.dark15Text}>First Occured: {moment(this.props.firstOccured).format('DD MMM YYYY h:mm:ss a')}</Text>
-          <Text style={styles.dark15Text}>OCCURANCES</Text>
-          <BarChart data={this.props.dailyOccurances} start={this.props.firstOccured} end={this.props.lastOccured} numberType='number' /> 
-        </View>
-        <View style={[styles.app, {height: 200}]}>
-          <PieChart data={this.state.version} />
-        </View>
+        <ScrollView>
+          <View style={styles.app}>
+            <Text style={styles.dark15Text}>CRASH DETAILS</Text>
+            <Text style={styles.bold15Text}>{this.props.crashName}</Text>
+            <Text style={styles.dark15Text}>{this.props.reason}</Text>
+            <Text style={styles.dark15Text}>Status: {this.props.status}</Text>
+            <Text style={styles.dark15Text}>Occurred: {this.props.occurances} times</Text>
+            <Text style={styles.dark15Text}>Affected: {this.props.users} users</Text>
+            <Text style={styles.dark15Text}>Last Occured: {moment(this.props.lastOccured).format('DD MMM YYYY h:mm:ss a')}</Text>
+            <Text style={styles.dark15Text}>First Occured: {moment(this.props.firstOccured).format('DD MMM YYYY h:mm:ss a')}</Text>
+            <Text style={styles.dark15Text}>OCCURANCES</Text>
+            <BarChart data={this.props.dailyOccurances} start={this.props.firstOccured} end={this.props.lastOccured} numberType='number' /> 
+          </View>
+          <View style={[styles.app, {height: 200}]}>
+            <View style={[{flexDirection: 'row'}, {justifyContent: 'space-around'}]}>
+              <Text style={styles.dark15Text}>App Versions</Text>
+              <Text style={styles.dark15Text}>OS Versions</Text>
+              <Text style={styles.dark15Text}>Devices</Text>
+            </View>
+            <View style={[{marginTop: 40}, {marginLeft: 53}]}>
+              <PieChart data={this.state.version} />
+            </View>
+            <CrashList data={this.state.version} />
+          </View>
+        </ScrollView>
       </View>
     )
   }
@@ -61,5 +71,39 @@ class CrashDetail extends Component {
     this.props.navigator.pop();
   }
 };
+
+class CrashList extends Component {
+  _getDetails(){
+    let allDetails = [];
+    let total = Math.ceil(this.props.data.reduce((n, d) => d.value + n, 0))
+    for(var i = 0; i < this.props.data.length; i++){
+      let data = this.props.data[i];
+      let percent = data['value'] / total;
+      //console.log(percent);
+      allDetails.push(<CrashItem key={[i]} name={data['label']} value={data['value']} percent={percent} />);
+    }
+    return allDetails
+  }
+
+  render(){
+    return(
+      <View style={styles.app}>
+        {this._getDetails()}
+      </View>
+    )
+  }
+}
+
+class CrashItem extends Component {
+  render(){
+    return(
+      <View style={[{flexDirection: 'row'}, {justifyContent: 'space-between'}]}>
+        <Text>{this.props.name}</Text>
+        <Text>{this.props.value}</Text>
+        <Text>{numeral(this.props.percent).format('0.0000')+'%'}</Text>
+      </View>
+    )
+  }
+}
 
 module.exports = CrashDetail;
