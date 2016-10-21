fb00797f610296cb7eb6389bdcfefe93b8428589
Sarah Scott <s.j.scott84@gmail.com>
2016-10-21 13:11:30 -0700

    Display data on interactive pie chart

--- src/components/interactivePieChart.js
+++ src/components/interactivePieChart.js
@@ -18,6 +18,15 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 class InteractivePieChart extends Component {
+  constructor(){
+    super();
+    this.state = {
+      label: '',
+      value: '',
+      percent: ''
+    }
+  };
+
   render(){
     return(
       <View style={styles.container}>
@@ -27,11 +36,32 @@ class InteractivePieChart extends Component {
           <Icon.Button name="cog" size={20} color='rgb(98,129,133)' backgroundColor='white' onPress={this._onPressBack.bind(this)} />
         </View>
         <View style={styles.app}>
-          <PieChart data={this.props.data} height={width-24} width={width-24} cx={(width-24)/2} cy={(width-24)/2} interactive={true} />
+          <PieChart data={this.props.data} height={width-24} width={width-24} cx={(width-24)/2} cy={(width-24)/2} interactive={true} onPress={this._onPress} />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.dark15Text}>Name</Text>
+            <Text style={styles.bold15Text}>{this.state.label}</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.dark15Text}>Crashes</Text>
+            <Text style={styles.bold15Text}>{this.state.value}</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.dark15Text}>Percentage</Text>
+            <Text style={styles.bold15Text}>{numeral(this.state.percent).format('0.00%')}</Text>
+          </View>
         </View>
       </View>
     )
   };
+
+  _onPress = (label, value) => {
+    const dataTotal = Math.ceil(this.props.data.reduce((n, d) => d.value + n, 0));
+    this.setState({
+      label: label,
+      value: value,
+      percent: value / dataTotal
+    })
+  }
 
   _getCircleDimensions(){
     let dimension;
