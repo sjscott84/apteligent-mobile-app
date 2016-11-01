79b5d93ee49bfacb7fa580d3e11f05be9cd2f24a
Sarah Scott <s.j.scott84@gmail.com>
2016-11-01 15:17:16 -0700

    Amend the footer buttons

--- src/components/appFooter.js
+++ src/components/appFooter.js
@@ -17,13 +17,13 @@ class AppFooter extends Component {
   render (){
     return (
       <View style={[styles.footer, {flexDirection: 'row'}, {justifyContent: 'space-around'}]}>
-        <TouchableHighlight onPress={this._onPressCrashSummary.bind(this)}>
+        <TouchableHighlight onPress={this._onPressNewRoute.bind(this, 'crashInfo', {id: this.props.id, name: this.props.name, type: this.props.type, time: '1', version: 'all'})}>
           <View style={[{flexDirection: 'column'}, {alignItems: 'center'}]}>
             <Icon style={{marginLeft: 15}} name="exclamation-triangle" size={15} color='rgb(122,143,147)' backgroundColor='white' />
             <Text style={styles.light13Text} >Crash Summary</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight onPress={this._onPressAppOverview.bind(this)}>
+        <TouchableHighlight onPress={this._onPressNewRoute.bind(this, 'appDetails', {id: this.props.id, name: this.props.name, type: this.props.type})}>
           <View style={[{flexDirection: 'column'}, {alignItems: 'center'}]}>
             <Icon style={{marginLeft: 15}} name="pie-chart" size={15} color='rgb(122,143,147)' backgroundColor='white' />
             <Text style={styles.light13Text}>App Overview</Text>
@@ -33,28 +33,14 @@ class AppFooter extends Component {
     )
   };
 
-  _onPressCrashSummary(){
-    this.props.navigator.push({
-      name: 'crashInfo',
-      passProps: {
-        id: this.props.id,
-        name: this.props.name,
-        type: this.props.type,
-        time: '1',
-        version: 'all'
-      }
-    })
-  }
-
-  _onPressAppOverview(){
-    this.props.navigator.push({
-      name: 'appDetails',
-      passProps: {
-        id: this.props.id,
-        name: this.props.name,
-        type: this.props.type
-      }
-    })
+  _onPressNewRoute(newRoute, props){
+    let currentRoute = this.props.navigator.state.routeStack[this.props.navigator.state.routeStack.length - 1];
+    if(currentRoute.name !== newRoute){
+      this.props.navigator.push({
+        name: newRoute,
+        passProps: props
+      })
+    }
   }
 };
 
