008ee1d0fc9c21ef734fc0bb3d33b61ab56a2e97
Sarah Scott <s.j.scott84@gmail.com>
2016-11-17 10:57:17 -0800

    Remove all old commented out code

--- src/library/api.js
+++ src/library/api.js
@@ -33,251 +33,69 @@ provideAccessToken = function(token, callback){
   callback();
 };
 
-//Get a list of available apps
-getAppsList = function(callback){
+//Handle the api calls
+apiCall = function(url, callback){
   const myHeaders = new Headers({
     "Authorization": 'Bearer '+ACCESS_ID
   });
-  var request = new Request('https://developers.crittercism.com:443/v2/apps?attributes=appName%2CappType%2CappVersions%2CiconURL', {
+  var request = new Request(url, {
     method: 'GET',
     headers: myHeaders
   })
   fetch(request)
     .then((res) => {
-      if(res.ok){
         return res.json();
-      }else{
-        console.log('Error')
-      }
     })
     .then((json) => {
-      callback(json.data);
+      //console.log(json.data);
+      callback(null, json.data);
     })
     .catch((err) => {
-      callback("Error");
-      console.log(err);
+      callback(err);
     })
+}
+
+//Get a list of available apps
+getAppsList = function(callback){
+  apiCall('https://developers.crittercism.com:443/v2/apps?attributes=appName%2CappType%2CappVersions%2CiconURL', callback);
 }
 
 //Get some very basic crash information regarding specific ids
 getCrashSummariesApi = function(id, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/liveStats/'+id+'/P1D', {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err)
-    })
+  apiCall('https://developers.crittercism.com:443/v2/liveStats/'+id+'/P1D', callback);
 }
 
 //Get info from the livestats api
 getLiveStateData = function(id, time, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/liveStats/'+id+'/'+time, {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err)
-    })
+  apiCall('https://developers.crittercism.com:443/v2/liveStats/'+id+'/'+time, callback);
 }
-
-/*getCrashRateGraphInfo = function(id, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/errorMonitoring/graph?appId='+id+'&graph=crashPercent&duration=41760', {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-}*/
 
 //Get a list of all crash groups for a specic app
 getCrashInfoGeneral = function(id, time, version, sort, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/crash/paginatedtable/'+id+'?appVersion='+version+'&period='+time+'&sortBy='+sort+'&sortOrder=descending', {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        //callback("Error");
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err);
-    })
+  apiCall('https://developers.crittercism.com:443/v2/crash/paginatedtable/'+id+'?appVersion='+version+'&period='+time+'&sortBy='+sort+'&sortOrder=descending', callback);
 }
 
 //Get more details regarding a specific crash
 getCrashInfoDetail = function(id, hash, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/crash/'+id+'/'+hash+'?diagnostics=false&get_other_crashes=false', {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err);
-    })
+  apiCall('https://developers.crittercism.com:443/v2/crash/'+id+'/'+hash+'?diagnostics=false&get_other_crashes=false', callback);
 }
 
 //Get Monthly Active Users from api
 getMAUFromApi = function(id, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/trends/mau/'+id, {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      console.log(err);
-      callback("Error");
-    })
+  apiCall('https://developers.crittercism.com:443/v2/trends/mau/'+id, callback);
 }
 
 //Get daily active users from api
 getDAUFromApi = function(id, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/trends/dau/'+id, {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err);
-    })
+  apiCall('https://developers.crittercism.com:443/v2/trends/dau/'+id, callback);
 }
 
 //Get all app versions from api
 getAppVersions = function(callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/apps?attributes=appVersions', {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+  apiCall('https://developers.crittercism.com:443/v2/apps?attributes=appVersions', callback);
 }
 
 //Get user details from the api
 getUserDetailsApi = function(id, hash, callback){
-  const myHeaders = new Headers({
-    "Authorization": 'Bearer '+ACCESS_ID
-  });
-  var request = new Request('https://developers.crittercism.com:443/v2/crash/userData/'+id+'/'+hash, {
-    method: 'GET',
-    headers: myHeaders
-  })
-  fetch(request)
-    .then((res) => {
-      if(res.ok){
-        return res.json();
-      } else {
-        console.log('error');
-      }
-    })
-    .then((json) => {
-      callback(json);
-    })
-    .catch((err) => {
-      callback("Error");
-      console.log(err);
-    })
+  apiCall('https://developers.crittercism.com:443/v2/crash/userData/'+id+'/'+hash, callback);
 }
